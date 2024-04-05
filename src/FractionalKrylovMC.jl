67dded950953b59e23a5cc33a735963fbb620d89
aner <4aner4@gmail.com>
2024-04-05 15:36:35 +0200

    Files generated by PkgTemplates
    PkgTemplates version: 0.7.48

---
+++ src/FractionalKrylovMC.jl
@@ -0,0 +1,5 @@
+module FractionalKrylovMC
+
+# Write your package code here.
+
+end
---
+++ test/runtests.jl
@@ -0,0 +1,6 @@
+using FractionalKrylovMC
+using Test
+
+@testset "FractionalKrylovMC.jl" begin
+    # Write your tests here.
+end
