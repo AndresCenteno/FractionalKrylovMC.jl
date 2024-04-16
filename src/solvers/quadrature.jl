247e31ecc86c546d412e11ccf20a88c48db33b15
aner <4aner4@gmail.com>
2024-04-16 17:11:15 +0100

    he aislado el error

--- src/solvers/quadrature.jl
+++ src/solvers/quadrature.jl
@@ -5,24 +5,24 @@ over [0,1]^3
 
 struct QuadSolver <: MatVecSolver end
 
-function solve(problem::MittagLefflerProblem{T},::QuadSolver;atol=1e-5,rtol=1e-2) where T<:Real
+function solve(problem::MittagLefflerProblem{T},::QuadSolver;atol=1e-5,rtol=1e-2,rightΔ=sqrt(eps())) where T<:Real
     # unpack parameters
     Anμ = problem.Anμ; n = problem.nμ; γ = problem.γ; α = problem.α; t = problem.t; u0 = problem.u0
     TotalRNG(p,u) = SpectralKernelRNG(p[1],t^p[1],u[1])^(p[1]*n/p[2])*stblrndsub(p[2]/(p[1]*n),u[2],u[3])
     dTotalRNGdp(p,u) = ForwardDiff.gradient(p->TotalRNG(p,u),p)
-    uT = hcubature(u->exp(-Anμ*TotalRNG([α,γ],u))*u0,[0;0;0],[1;1;1],atol=atol,rtol=rtol)[1]
-    duTdp = (-Anμ)*hcubature(u->exp(-Anμ*TotalRNG([α;γ],u))*u0*dTotalRNGdp([α;γ],u)',[0;0;0],[1;1;1],atol=atol,rtol=rtol)[1]
+    uT = hcubature(u->exp(-Anμ*TotalRNG([α,γ],u))*u0,[0;0;0],(1-rightΔ).*[1;1;1],atol=atol,rtol=rtol)[1]
+    duTdp = (-Anμ)*hcubature(u->exp(-Anμ*TotalRNG([α;γ],u))*u0*dTotalRNGdp([α;γ],u)',[0;0;0],(1-rightΔ).*[1;1;1],atol=atol,rtol=rtol)[1]
     return MittagLefflerSolution(uT,duTdp[:,1],duTdp[:,2])
 end
 
 struct QuadKrySolver <: MatVecSolver end
 
-function solve(problem::MittagLefflerProblem{T},KryDim::Int,::QuadKrySolver;atol=1e-5,rtol=1e-2) where T<:Real
+function solve(problem::MittagLefflerProblem{T},KryDim::Int,::QuadKrySolver;atol=1e-5,rtol=1e-2,rightΔ=sqrt(eps())) where T<:Real
     # unpack parameters
     Anμ = problem.Anμ; n = problem.nμ; γ = problem.γ; α = problem.α; t = problem.t; u0 = problem.u0
     TotalRNG(p,u) = SpectralKernelRNG(p[1],t^p[1],u[1])^(p[1]*n/p[2])*stblrndsub(p[2]/(p[1]*n),u[2],u[3])
     dTotalRNGdp(p,u) = ForwardDiff.gradient(p->TotalRNG(p,u),p)
-    uT = hcubature(u->expmv(TotalRNG([α;γ],u),-Anμ,u0,m=KryDim),[0;0;0],[1;1;1],atol=atol,rtol=rtol)[1]
+    uT = hcubature(u->expmv(TotalRNG([α;γ],u),-Anμ,u0,m=KryDim),[0;0;0],(1-rightΔ).*[1;1;1],atol=atol,rtol=rtol)[1]
     duTdp = (-Anμ)*hcubature(u->expmv(TotalRNG([α;γ],u),-Anμ,u0,m=KryDim)*dTotalRNGdp([α;γ],u)',[0;0;0],[1;1;1],atol=atol,rtol=rtol)[1]
     return MittagLefflerSolution(uT,duTdp[:,1],duTdp[:,2])
 end
--- test/NaN_tests.jl
+++ test/NaN_tests.jl
@@ -1,19 +1,34 @@
 # checking where the NaNs come from
 
-using FractionalKrylovMC
+using FractionalKrylovMC, HCubature
 
 flag = 1
 k = 30
+
+## KRYLOV
 while true
     @show flag
     flag += 1
     global problem = create_random_problem(rand()*0.9+0.1,rand()*0.9+0.1,k,MittagLefflerRand())
     try
-        sol = solve(problem,10,QuadKrySolver())
+        sol = solve(problem,QuadSolver())
     catch
         break
     end
 end
 
-@run sol = solve(problem,10,QuadKrySolver())
-
+# STANDARD
+while true
+    @show flag
+    flag += 1
+    global problem = create_random_problem(rand()*0.9+0.1,rand()*0.9+0.1,k,MittagLefflerRand())
+    sol = solve(problem,QuadSolver(),rightΔ=1e-5)
+    if any(isnan.(sol.uT)); break; end
+end
+sol = solve(problem,QuadSolver(),rightΔ=1e-5)
+sol.uT
+rightΔ = 1e-5
+Anμ = problem.Anμ; n = problem.nμ; γ = problem.γ; α = problem.α; t = problem.t; u0 = problem.u0
+TotalRNG(p,u) = SpectralKernelRNG(p[1],t^p[1],u[1])^(p[1]*n/p[2])*stblrndsub(p[2]/(p[1]*n),u[2],u[3])
+dTotalRNGdp(p,u) = ForwardDiff.gradient(p->TotalRNG(p,u),p)
+uT = hcubature(u->exp(-Anμ*TotalRNG([α,γ],u))*u0,[0;0;0],(1-rightΔ).*[1;1;1],atol=1e-5,rtol=1e-2)[1]
